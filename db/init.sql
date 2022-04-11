cfe463890da765be508c84dab9f215f553e546c7
Didrik Nordström <diddenordstrom@gmail.com>
2022-04-11 13:43:21 +0200

    docker starts its own instance of mysql

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,7 @@
-CREATE TABLE test(
-    id IN USIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50)
-)
+CREATE TABLE users(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    userPassword VARCHAR(100),
+    email VARCHAR(50),
+    constraint usernameUnique UNIQUE (username)
+);
