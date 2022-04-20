5d96197f35329bf4591061381126ebad711b6e3b
Didrik Nordström <diddenordstrom@gmail.com>
2022-04-20 14:12:17 +0200

    function to label images started
    Feature! Not yet done, cannot authorize the google vision api with our registered account :8ball:
    
    Co-Authored-By: axwellfred <56869240+axwellfred@users.noreply.github.com>

--- src/PL/routes/standard-router.js
+++ src/PL/routes/standard-router.js
@@ -1,10 +1,22 @@
 const express = require("express")
+const vision = require("@google-cloud/vision")
 
 module.exports = function() {
     const router = express.Router()
 
-    router.get("/", function(req, res){
-        res.json("hello world")
+    router.get("/", async function(req, res){
+
+        const client = new vision.ImageAnnotatorClient();
+
+        // Performs label detection on the image file
+        const [result] = await client.labelDetection('https://produits.bienmanger.com/36700-0w470h470_Organic_Red_Onion_From_Italy.jpg')
+        const labels = result.labelAnnotations;
+        console.log('Labels:');
+        labels.forEach(label => console.log(label.description));
+
+
+
+        res.json(labels)
     })
     return router
 }
