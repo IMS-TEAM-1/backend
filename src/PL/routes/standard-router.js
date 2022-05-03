94029fc8a60d00f75c2f5fac2676aa8174675c11
axwellfred <axel.fredholm@yahoo.com>
2022-05-03 22:47:09 +0200

    Authentication credentials

--- src/PL/routes/standard-router.js
+++ src/PL/routes/standard-router.js
@@ -1,12 +1,20 @@
 const express = require("express")
 const vision = require("@google-cloud/vision")
+const { config } = require("dotenv")
 
 module.exports = function() {
     const router = express.Router()
 
     router.get("/", async function(req, res){
 
-        const client = new vision.ImageAnnotatorClient();
+        const {privateKey} = JSON.parse(process.env.GOOGLE_VISION_PRIVATE_KEY);
+        const {clientEmail} = JSON.parse(process.env.GOOGLE_VISION_EMAIL);
+        const client = new vision.ImageAnnotatorClient({
+            credentials: {
+                private_key: privateKey,
+                client_email: clientEmail,
+            }
+        });
 
         // Performs label detection on the image file
         const [result] = await client.labelDetection('https://produits.bienmanger.com/36700-0w470h470_Organic_Red_Onion_From_Italy.jpg')
