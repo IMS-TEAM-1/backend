f174a40e4a06cc8f0a335926db39ca955d82051d
Didrik Nordström <diddenordstrom@gmail.com>
2022-05-19 11:04:34 +0200

    Update 20220411075237_create_mower_location_image_table.js

--- src/database/migrations/20220411075237_create_mower_location_image_table.js
+++ src/database/migrations/20220411075237_create_mower_location_image_table.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('mower_location_image', function(table) {
         table.increments();
         table.integer('mower_location_id').unsigned().notNullable();
-        table.binary('image');
+        table.binary('image', 65536);
         table.string('classification');
 
         table.foreign('mower_location_id')
